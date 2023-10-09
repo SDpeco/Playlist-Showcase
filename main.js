8cf7a16f2e8f67a7c404f3f20aa1f5853619ee86
JulianPeressini <decime-julian@hotmail.com>
2023-10-09 06:12:52 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,100 @@
+const grid = document.querySelector("[data-grid]");
+const headerImage = document.querySelector("[data-header-image]");
+const albumTitle = document.querySelector("[data-album-title]");
+const albumAtist = document.querySelector("[data-album-artist]");
+const headerContainer = document.querySelector("[data-header-container]");
+const albumLink = document.querySelector("[data-album-link]");
+const albumFlavor = document.querySelector("[data-flavor]");
+
+const greenFlavor = "It do be bopping.";
+const purpleFlavor = "Quite interesting, give it a listen.";
+
+let previousEvent = null;
+
+fetch("./albums.json")
+  .then(function (resp) {
+    return resp.json();
+  })
+  .then(function (data) {
+    headerImage.setAttribute("src", data.albums[data.albums.length - 1].image);
+    albumTitle.innerHTML = data.albums[data.albums.length - 1].name;
+    albumAtist.innerHTML = data.albums[data.albums.length - 1].artist;
+    albumLink.setAttribute("href", data.albums[data.albums.length - 1].link);
+
+    switch (data.albums[data.albums.length - 1].highlight) {
+      case "green":
+        headerImage.classList.add("green-album");
+        albumFlavor.classList.add("green-flavor");
+        albumFlavor.innerHTML = greenFlavor;
+        break;
+      case "purple":
+        headerImage.classList.add("purple-album");
+        albumFlavor.classList.add("purple-flavor");
+        albumFlavor.innerHTML = purpleFlavor;
+        break;
+    }
+
+    for (let i = data.albums.length - 1; i > -1; i--) {
+      let newGridElement = document.createElement("img");
+      newGridElement.classList.add("grid-item", "grid-id-" + i);
+
+      switch (data.albums[i].highlight) {
+        case "green":
+          newGridElement.classList.add("green-album");
+          break;
+        case "purple":
+          newGridElement.classList.add("purple-album");
+          break;
+        default:
+      }
+
+      newGridElement.src = data.albums[i].image;
+      newGridElement.addEventListener("click", (event) => {
+        headerImage.setAttribute(
+          "src",
+          event.currentTarget.getAttribute("src")
+        );
+
+        let albumId = event.currentTarget.classList[1];
+        albumId = albumId.substring(8, 999);
+
+        albumFlavor.innerHTML = "";
+        headerImage.classList.remove("green-album");
+        albumFlavor.classList.remove("green-flavor");
+        headerImage.classList.remove("purple-album");
+        albumFlavor.classList.remove("purple-flavor");
+        headerImage.classList.remove("bronze-album");
+        albumFlavor.classList.remove("bronze-flavor");
+        headerImage.classList.remove("silver-album");
+        albumFlavor.classList.remove("silver-flavor");
+        headerImage.classList.remove("gold-album");
+        albumFlavor.classList.remove("gold-flavor");
+
+        switch (data.albums[albumId].highlight) {
+          case "green":
+            headerImage.classList.add("green-album");
+            albumFlavor.classList.add("green-flavor");
+            albumFlavor.innerHTML = greenFlavor;
+            break;
+          case "purple":
+            headerImage.classList.add("purple-album");
+            albumFlavor.classList.add("purple-flavor");
+            albumFlavor.innerHTML = purpleFlavor;
+            break;
+        }
+
+        if (previousEvent != null) {
+          previousEvent.target.classList.remove("album-selected");
+        }
+
+        previousEvent = event;
+        event.currentTarget.classList.add("album-selected");
+
+        albumTitle.innerHTML = data.albums[albumId].name;
+        albumAtist.innerHTML = data.albums[albumId].artist;
+        albumLink.setAttribute("href", data.albums[albumId].link);
+      });
+
+      grid.appendChild(newGridElement);
+    }
+  });
